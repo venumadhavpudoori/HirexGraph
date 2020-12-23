4e3a585ad93e19e50503d197eda42e3e46860e4d
Venumadhav Pudoori <venumadhavpudoori@gmail.com>
2020-12-23 10:58:10 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import LineChart from "./components/LineChart";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="chart">
+        <LineChart />
+      </div>
+    </div>
+  );
+}
+
+export default App;
